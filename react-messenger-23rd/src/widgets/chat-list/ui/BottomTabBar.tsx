a75da32604d5e318133a03caaf01ef1bdf9173ac
a-00-a <sy.lee.2391@gmail.com>
2026-04-06 23:32:02 +0900

    Feat: 하단 탭바 active 상태에 따른 아이콘 적용

--- react-messenger-23rd/src/widgets/chat-list/ui/BottomTabBar.tsx
+++ react-messenger-23rd/src/widgets/chat-list/ui/BottomTabBar.tsx
@@ -2,15 +2,18 @@ import { Link, useLocation } from 'react-router-dom';
 
 import navIcon from '@/shared/assets/icons/chat-list/compass-03.svg';
 import chattingIcon from '@/shared/assets/icons/chat-list/Icon.svg';
+import activeChattingIcon from '@/shared/assets/icons/chat-list/Icon-active.svg';
 import myProfileIcon from '@/shared/assets/icons/chat-list/user-circle.svg';
+import activeMyProfileIcon from '@/shared/assets/icons/chat-list/user-circle-active.svg';
 import partnerIcon from '@/shared/assets/icons/chat-list/users-01.svg';
+import activePartnerIcon from '@/shared/assets/icons/chat-list/users-01-active.svg';
 
 const BottomTabBar = () => {
   const location = useLocation();
 
-  const isChatPage = location.pathname === '/' || location.pathname.startsWith('/chat/');
-  const isFriendsPage = location.pathname === '/friends';
-  const isMyProfilePage = location.pathname === '/my-profile';
+  const isChatPage = location.pathname === '/' || location.pathname.startsWith('/chat');
+  const isFriendsPage = location.pathname.startsWith('/friends');
+  const isMyProfilePage = location.pathname.startsWith('/my-profile');
 
   const unreadCount = 1000;
   const badgeText = unreadCount > 999 ? '999+' : unreadCount;
@@ -20,7 +23,7 @@ const BottomTabBar = () => {
       <div className="flex items-end justify-between">
         <Link to="/" className="flex w-12 flex-col items-center gap-1 p-1">
           <div className="relative">
-            <img src={chattingIcon} alt="채팅" className="h-6 w-6" />
+            <img src={isChatPage ? activeChattingIcon : chattingIcon} alt="채팅" className="h-6 w-6" />
 
             {unreadCount > 0 && (
               <div className="absolute -top-1 left-3 px-1 min-w-[22px] h-5 rounded-[24px] bg-main flex items-center justify-center">
@@ -33,7 +36,7 @@ const BottomTabBar = () => {
         </Link>
 
         <Link to="/friends" className="flex w-12 flex-col items-center gap-1 p-1">
-          <img src={partnerIcon} alt="대화상대" className="h-6 w-6" />
+          <img src={isFriendsPage ? activePartnerIcon : partnerIcon} alt="대화상대" className="h-6 w-6" />
           <span className={isFriendsPage ? 'Caption03SB text-gray-80' : 'Caption03SB text-gray-60'}>대화 상대</span>
         </Link>
 
@@ -43,7 +46,7 @@ const BottomTabBar = () => {
         </button>
 
         <Link to="/my-profile" className="flex w-12 flex-col items-center gap-1 p-1">
-          <img src={myProfileIcon} alt="내 프로필" className="h-6 w-6" />
+          <img src={isMyProfilePage ? activeMyProfileIcon : myProfileIcon} alt="내 프로필" className="h-6 w-6" />
           <span className={isMyProfilePage ? 'Caption03SB text-gray-80' : 'Caption03SB text-gray-60'}>내 프로필</span>
         </Link>
       </div>
