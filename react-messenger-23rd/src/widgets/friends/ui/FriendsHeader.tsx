6fbb736f1835199b41067701d52ec01fe5484f41
a-00-a <sy.lee.2391@gmail.com>
2026-04-08 15:38:58 +0900

    Feat: 친구목록 헤더 UI 구현

--- react-messenger-23rd/src/widgets/friends/ui/FriendsHeader.tsx
+++ react-messenger-23rd/src/widgets/friends/ui/FriendsHeader.tsx
@@ -1,12 +1,22 @@
+import questionIcon from '@/shared/assets/icons/friends/message-question-circle(24_24).svg';
+import searchIcon from '@/shared/assets/icons/friends/search(24_24).svg';
+import addFriendIcon from '@/shared/assets/icons/friends/user-plus(24_24).svg';
+
 const FriendsHeader = () => {
   return (
     <header className="flex items-center justify-between px-4 py-5">
       <h1 className="Heading01SB text-gray-90">대화 상대</h1>
 
       <div className="flex items-center gap-4">
-        <button className="h-6 w-6" />
-        <button className="h-6 w-6" />
-        <button className="h-6 w-6" />
+        <button type="button" className="flex h-6 w-6 items-center justify-center">
+          <img src={searchIcon} alt="친구검색" className="h-6 w-6" />
+        </button>
+        <button type="button" className="flex h-6 w-6 items-center justify-center">
+          <img src={addFriendIcon} alt="친구추가" className="h-6 w-6" />
+        </button>
+        <button type="button" className="flex h-6 w-6 items-center justify-center">
+          <img src={questionIcon} alt="물음표" className="h-6 w-6" />
+        </button>
       </div>
     </header>
   );
